f0bcbeca33b6b7b9d0b44553bd24864bc15f0368
microbioSaroj <62801451+microbioSaroj@users.noreply.github.com>
2026-01-05 04:49:54 +0000

    Deploying to gh-pages from @ microbioSaroj/microbiosaroj.github.io@4f6030ebe0ca99ef71306fedeb5eaca766d3a685 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "microbioSaroj/sarojkhadka",
+    "data-repo": "microbioSaroj/microbiosaroj.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
