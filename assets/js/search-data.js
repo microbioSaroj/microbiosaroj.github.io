a3f76d3ee520c2b08d0e7bb021d70290e73a0324
microbioSaroj <62801451+microbioSaroj@users.noreply.github.com>
2026-01-07 05:20:32 +0000

    Deploying to gh-pages from @ microbioSaroj/microbiosaroj.github.io@8dc15dc5507c8a1d535088db45186f52a2ce03d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-resume",
-          title: "Resume",
+  },{id: "nav-cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
