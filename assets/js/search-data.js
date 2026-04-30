8294a5ebdece368f3eb9926ee9edf593f12e5e8a
microbioSaroj <62801451+microbioSaroj@users.noreply.github.com>
2026-04-30 02:35:29 +0000

    Deploying to gh-pages from @ microbioSaroj/microbiosaroj.github.io@debea7e4bab117b01699b99de0f17fd600765f0c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of Saroj&#39;s research and personal projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
